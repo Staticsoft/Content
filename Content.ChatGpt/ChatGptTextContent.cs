4a4d95ffaeeebc30afb2d3c9d9af08855fc3e6ca
Oleg Rakovitch <devgeslot@gmail.com>
2024-01-03 22:56:11 -0500

    Chat GPT text content

---
+++ Content.ChatGpt/ChatGptTextContent.cs
@@ -0,0 +1,33 @@
+﻿using OpenAI.Interfaces;
+using OpenAI.ObjectModels;
+using OpenAI.ObjectModels.RequestModels;
+using Staticsoft.Content.Abstractions;
+
+namespace Staticsoft.Content.ChatGpt;
+
+public class ChatGptTextContent(
+    IOpenAIService chatGpt
+) : TextContent
+{
+    readonly IOpenAIService ChatGpt = chatGpt;
+
+    public async Task<string> Produce(string requirements)
+    {
+        var completionResult = await ChatGpt.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+        {
+            Messages = new List<ChatMessage>
+            {
+                ChatMessage.FromUser(requirements)
+            },
+            Model = Models.Gpt_4
+        });
+
+        if (completionResult.Error != null) throw new ContentException(completionResult.Error.Message);
+
+        return Normalize(completionResult.Choices.First().Message.Content);
+    }
+
+    static string Normalize(string text) => text
+        .Replace(Environment.NewLine, "\n")
+        .Replace("\n", Environment.NewLine);
+}
---
+++ Content.ChatGpt/DependencyInjectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenAI.Extensions;
+using Staticsoft.Content.Abstractions;
+
+namespace Staticsoft.Content.ChatGpt;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection UseChatGpt(this IServiceCollection services, string apiKey) => services
+            .AddSingleton<TextContent, ChatGptTextContent>()
+            .AddOpenAIService(settings => { settings.ApiKey = apiKey; }).Services;
+}
